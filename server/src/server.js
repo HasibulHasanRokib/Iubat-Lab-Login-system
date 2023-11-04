708da460fa5df9834a8851dfd2518d05ab3e75b2
Hasibul Hasan Rokib <134086135+HasibulHasanRokib@users.noreply.github.com>
2023-11-05 03:46:30 +0600

    update

--- server/src/server.js
+++ server/src/server.js
@@ -1,7 +1,7 @@
 const app=require("./app");
 const connectDB = require("./db");
 
-const PORT=process.env.PORT;
+const PORT=process.env.PORT || 3000;
 
 app.listen(PORT,async()=>{
     console.log(`server is running.`)
