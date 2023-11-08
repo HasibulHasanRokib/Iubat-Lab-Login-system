321aa2cc8b421187ec4a7276b5331b0b08ba4db5
Hasibul Hasan Rokib <134086135+HasibulHasanRokib@users.noreply.github.com>
2023-11-09 00:20:29 +0600

    NOV 9

--- server/src/server.js
+++ server/src/server.js
@@ -1,9 +1,9 @@
 const app=require("./app");
 const connectDB = require("./db");
 
-const PORT=3000;
+const PORT= process.env.PORT||3000;
 
 app.listen(PORT,async()=>{
-    console.log(`server is running at http://localhost:${PORT}`)
+    console.log(`server is running .`)
     await connectDB()
 })
